4ab8a79f9df2e4a7d99d6153a05727071174656a
Viny <Viny@Diego>
2016-04-27 11:28:57 -0500

    segunda modificacion

--- Fisei_Grupo/src/fisei_grupo/Fisei_Grupo.java
+++ Fisei_Grupo/src/fisei_grupo/Fisei_Grupo.java
@@ -16,6 +16,7 @@ public class Fisei_Grupo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hola primera modificacion");
     }
     
 }
